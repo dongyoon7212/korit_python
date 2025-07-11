730e231ba3a9305153b48738fe3f5c9f4046b515
dongyoon7212 <dongyoon7212@naver.com>
2025-07-11 13:27:44 +0900

    commit

---
+++ 12_modules/modules02.py
@@ -0,0 +1,18 @@
+# requests 모듈
+# requests는 python에서 HTTP 요청을 보내는 데 사용되는 라이브러리다.
+# import requests
+#
+# response = requests.get("https://www.naver.com/")
+# print(response.status_code) #웹 서버로부터 받은 응답의 HTTP 상태코드를 출력
+# print(response.text)
+
+# pandas 모듈
+# pandas는 데이터 조작 및 분석을 위한 라이브러리다.
+# CSV, Excel, 데이터베이스 등 다양한 형식의 데이터를 읽고 쓸 수 있으며,
+# 데이터를 정렬, 필터링, 그룹화하는 기능을 제공합니다.
+import pandas as pd
+
+
+
+
+
