cf12f25d87a5c0801527e5bbd787006b7b2c8a4d
dongyoon7212 <dongyoon7212@naver.com>
2025-07-23 15:18:32 +0900

    tkinter

---
+++ 18_tkinter/tkinter03.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("회원가입")
+root.geometry("500x200")
+
+id_label = tk.Label(root, text="아이디:")
+id_label.grid(row=0, column=0, pady=10)
+
+id_entry = tk.Entry(root)
+id_entry.grid(row=0, column=1, padx=5)
+
+#entry옆에 버튼 하나 만들고 해당 버튼 클릭시
+#아이디 출력
+#아이디 밑에 비밀번호도 똑같이 만드세요
+
+
+root.mainloop()
